dcc5231955454b202ef5744c747cdcc0617ed19d
Rubicsiva <sivakathir19052001@gmail.com>
2024-12-05 10:35:19 +0530

    first commit

---
+++ backend/controller/payment/verify.js
@@ -0,0 +1,33 @@
+const { Cashfree } = require("cashfree-pg");
+
+async function verify(req,res){
+    try {
+        let {orderId}=req.body;
+        
+        Cashfree.PGOrderFetchPayments("2023-08-01",orderId).then((response)=>{
+            
+            res.json({
+                data:response.data[0],
+                error:false,
+                success:true,
+                message:"Order Verify successfully"
+            })
+        }).catch(err=>{
+            res.json({
+                message:err?.message || err,
+                error:true,
+                success:false
+            })
+            
+        })
+    } catch (err) {
+        res.json({
+            message:err?.message || err,
+            error:true,
+            success:false
+        })
+        
+    }
+}
+
+module.exports=verify
